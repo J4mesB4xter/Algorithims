65a8c2a76ae96312742f101d65215d818d5bd877
J4mesB4xter <67648868+J4mesB4xter@users.noreply.github.com>
2020-10-12 12:09:56 -0400

    Update FindMiddleElement.py
    added a description of the puzzle and an explanation for the wonkiness of the return line

--- Code-Wars/FindMiddleElement.py
+++ Code-Wars/FindMiddleElement.py
@@ -1,3 +1,9 @@
+#code wars tasked me with finding the middle element in lists 
+#of three, but after accomplishing that i went ahead and 
+#made a code that would work on larger lists as well.
+#a list of three is so specific that it seemed necessary to 
+#abstract and find a more reusable function
+
 def gimme(input_array):
     n = sorted(input_array)
     return input_array.index(n[round(len(input_array)/2)-1])
