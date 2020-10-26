e8fe777e5ea716b61305e100dfc9901227dacbf0
unknown <jbj1996@gmail.com>
2020-10-26 16:49:56 -0400

    added new solutions

--- Code-Wars/DetectPangram.py
+++ Code-Wars/DetectPangram.py
@@ -3,8 +3,9 @@ from string import ascii_lowercase
 
 def is_pangram(s):
     x = 0
+    y = s.lower()
     for i in string.ascii_lowercase:
-        if s.count(i) >= 1:
+        if y.count(i) >= 1:
             x = x + 1
     if x is 26:
         return True
