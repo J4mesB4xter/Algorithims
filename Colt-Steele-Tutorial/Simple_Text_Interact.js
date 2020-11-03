3a1e230b39f33de7826632f59cb87bdd00662795
unknown <jbj1996@gmail.com>
2020-11-03 10:52:13 -0500

    first foray into JS; made a button do a thing!

--- Colt-Steele-Tutorial/Simple_Text_Interact.js
+++ Colt-Steele-Tutorial/Simple_Text_Interact.js
@@ -1,10 +1,24 @@
-document.querySelector("button").addEventListener("click", function(){
-    document.body.style.background = randColor();
-})
+alert("One of these things is not like the other. One of these things does not beWONG!");
 
-function randColor(){
-    return '#' + (function co(lor){
-return (lor +=
-    [0,,2,3,4,5,6,7,8,9,'a','b','c','d','e','f'][Math.floor(Math.randon()*16)]) && (lor.length == 6) ?  lor : co(lor);
-    })('');
-}
+var pics = [
+    "imgs/Wong1.jpeg",
+    "imgs/Wong2.jpeg",
+    "imgs/Wong3.jpeg",
+    "imgs/Wong4.jpeg",
+    "imgs/Wong5.jpeg",
+    "imgs/Wong6.jpeg",
+    "imgs/Wong7.jpeg",
+    "imgs/Wong8.jpeg",
+    "imgs/POG.jpeg"
+];
+
+var button = document.querySelector("button");
+var img = document.querySelector("img");
+var counter = 0;
+button.addEventListener("click", function(){
+    if(counter === 9){
+        counter = 0;
+    }
+    img.src = pics[counter];
+    counter = counter + 1;
+});
