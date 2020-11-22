d6d7481fa7d4ff53f3f90e8189d42a49fb792f96
unknown <jbj1996@gmail.com>
2020-11-22 11:32:55 -0500

    .

--- Code-Wars/Python/BinaryCalculator.py
+++ Code-Wars/Python/BinaryCalculator.py
@@ -1,6 +1,5 @@
 def Calculate(a, b):
-    print("choose an operator (+, -, *, /")
-    y = input("")
+    y = input("choose an operator (+, -, *, /")
     if y == "+":
         return a + b
     elif y == "-":
@@ -11,5 +10,3 @@ def Calculate(a, b):
         return a / b
     else:
         return "invalid operator"
-
-Calculate(17, 18)
