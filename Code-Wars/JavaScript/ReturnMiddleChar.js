ccb52cc1f8dad8015d396f3dbc554fa5902404ff
unknown <jbj1996@gmail.com>
2020-12-06 20:38:14 -0500

    new function getmiddlechar

---
+++ Code-Wars/JavaScript/ReturnMiddleChar.js
@@ -0,0 +1,13 @@
+function getMiddle(s) {
+    console.log(s)
+    let x = s.split("").length;
+    let y = Number.isInteger(x/2);
+    let z = Math.floor(x/2)
+    if (x == 1) {
+      return s[0];
+    } else if (y == false) {
+      return s[z]
+    } else if (y == true) {
+      return s.substring(z-1, z+1)
+    }
+    }
