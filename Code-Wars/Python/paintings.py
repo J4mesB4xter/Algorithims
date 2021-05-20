beab806f4892ea853e4366741bb4d012805fef83
unknown <jbj1996@gmail.com>
2021-05-20 16:18:26 -0400

    added function to check for each color to have a presence greater than or equal to one

--- Code-Wars/Python/paintings.py
+++ Code-Wars/Python/paintings.py
@@ -10,8 +10,6 @@ def blank_painting():
     }
     return painting
 
-
-
 def two_reds(*paintings):
     red_count = 0
     for p in paintings:
@@ -40,10 +38,31 @@ def yellow_not_blue_green(*paintings):
                 return False
     return True
 
+def every_color(*paintings):
+    has_yellow = False
+    has_red = False
+    has_pink = False
+    has_green = False
+    has_blue = False
+    for p in paintings:
+        if p["yellow"]:
+            has_yellow = True
+        if p["red"]:
+            has_red = True
+        if p["pink"]:
+            has_pink = True
+        if p["green"]:
+            has_green = True
+        if p["blue"]:
+            has_blue = True
+    return has_pink and has_green and has_blue and has_yellow and has_red
+
 while True:
     p1 = blank_painting()
     p2 = blank_painting()
     p3 = blank_painting()
+    if not every_color(p1,p2, p3):
+        continue
     if not pink_not_green(p1, p2, p3):
         continue
     if not blue_pink(p1, p2, p3):
